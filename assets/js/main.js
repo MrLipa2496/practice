b7b09216b226aa8374893d835f4cea96853a2e10
MrLipa2496 <sasalipa216@gmail.com>
2025-03-20 17:12:54 +0200

    Add header and footer components dynamically

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Add header
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('components/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data
+    })
+    .catch(error => console.error('Error loading header:', error))
+
+  fetch('components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data
+    })
+    .catch(error => console.error('Error loading footer:', error))
+})
